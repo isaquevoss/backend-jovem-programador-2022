788b564a26b032eb48da872bbde72c025b182827
Isaque Voss Klassmann <isaque.iv@gmail.com>
2022-11-17 19:54:03 -0300

    tela de registro

--- 17-11-2022/twitter-frontend/src/views/Registro.vue
+++ 17-11-2022/twitter-frontend/src/views/Registro.vue
@@ -1,3 +1,27 @@
 <template>
-  <v-card> Registro </v-card>
+  <v-row>
+    <v-col>
+      <v-card>
+        <v-card-text>
+          <v-text-field label="Nome" v-model="usuario.nome"></v-text-field>
+          <v-text-field label="Email" v-model="usuario.email"></v-text-field>
+          <v-text-field
+            type="password"
+            label="Senha"
+            v-model="usuario.senha"
+          ></v-text-field>
+          <v-btn>Cadastrar</v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      usuario: {},
+    };
+  },
+};
+</script>
